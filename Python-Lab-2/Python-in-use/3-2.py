2da7360250041503d3e603cb1963643534e8a8ba
MohammadAmin <masz.netcom@hotmail.com>
2021-11-20 21:03:48 +0330

    loading masz.py module and use square func as square

---
+++ Python-Lab-2/Python-in-use/3-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 23:56:21 2021
+
+@author: MohammadAmin
+"""
+from masz import square
+x = input('enter number: ')
+print(square(x))
---
+++ Python-Lab-2/Python-in-use/masz.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 23:51:34 2021
+
+@author: MohammadAmin
+"""
+def square(x):
+    return float(x)**2
+    
